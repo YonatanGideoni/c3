88fe04528e46156fe34eccd4cbf1661b3b94d504
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2022-09-14 15:09:18 +0200

    Removed redundant functions

--- playground/brute_force_opt_gate.py
+++ playground/brute_force_opt_gate.py
@@ -3,16 +3,14 @@ import os
 import pickle
 import tempfile
 from copy import deepcopy
-from functools import reduce
 from typing import List
 
-import tensorflow as tf
 import numpy as np
 from matplotlib import pyplot as plt
 
+import c3.generator.devices as devices
 from c3.c3objs import Quantity
 from c3.experiment import Experiment
-import c3.generator.devices as devices
 from c3.generator.generator import Generator
 from c3.libraries import algorithms, hamiltonians, chip
 from c3.libraries.envelopes import envelopes
@@ -23,7 +21,7 @@ from c3.parametermap import ParameterMap
 from c3.signal import gates, pulse
 from c3.signal.gates import Instruction
 from c3.signal.pulse import Envelope
-from playground.plot_utils import wait_for_not_mouse_press
+from playground.plot_utils import wait_for_not_mouse_press, plot_dynamics, get_init_state
 
 SIDEBAND = 50e6
 
@@ -55,86 +53,10 @@ def setup_experiment_opt_ctrl(exp: Experiment, maxiter: int = 50) -> OptimalCont
     return opt
 
 
-def calc_exp_fid(exp: Experiment, index: list, dims: list) -> float:
+def calc_exp_fid(exp: Experiment) -> float:
+    dims = exp.pmap.model.dims
+    index = list(range(len(dims)))
     return (1 - unitary_infid_set(exp.propagators, exp.pmap.instructions, index, dims)).numpy()
-
-
-def plot_dynamics(exp, psi_init, seq, disp_legend: bool = False):
-    """
-    Plotting code for time-resolved populations.
-
-    Parameters
-    ----------
-    psi_init: tf.Tensor
-        Initial state or density matrix.
-    seq: list
-        List of operations to apply to the initial state.
-    """
-    model = exp.pmap.model
-    exp.compute_propagators()
-    dUs = exp.partial_propagators
-    psi_t = psi_init.numpy()
-    pop_t = exp.populations(psi_t, model.lindbladian)
-    for gate in seq:
-        for du in dUs[gate]:
-            psi_t = np.matmul(du.numpy(), psi_t)
-            pops = exp.populations(psi_t, model.lindbladian)
-            pop_t = np.append(pop_t, pops, axis=1)
-
-    ts = exp.ts
-    dt = ts[1] - ts[0]
-    ts = np.linspace(0.0, dt * pop_t.shape[1], pop_t.shape[1])
-    plt.plot(ts / 1e-9, pop_t.T)
-    plt.grid(linestyle="--")
-    plt.tick_params(
-        direction="in", left=True, right=True, top=True, bottom=True
-    )
-    plt.xlabel('Time [ns]')
-    plt.ylabel('Population')
-    if disp_legend:
-        plt.legend(model.state_labels, loc='center right')
-
-
-def plot_signal(drivers_signals: dict, t_final: float, awg: devices.AWG, n_points: int = 1000,
-                disp_legend: bool = False):
-    signal_t = np.linspace(0, t_final, n_points)
-
-    n_drivers = len(drivers_signals)
-    res_signal = np.zeros((n_drivers, len(signal_t)), dtype=np.complex128)
-    fig, axs = plt.subplots(1, n_drivers, sharey="all")
-
-    for driver, envs in drivers_signals.items():
-        driver_ind = int(driver[1:]) - 1
-        for env in envs:
-            # de-normalize the amplitude so it has units of volts
-            ts = awg.create_ts(0, t_final)
-            env.normalize_pulse = False
-            area = abs(env.get_shape_values(ts).numpy()).sum()
-            amplitude = env.params['amp'].numpy()
-            real_amplitude = amplitude / area
-
-            signal = env.get_shape_values(signal_t).numpy() * real_amplitude
-            res_signal[driver_ind] += signal
-
-            env.normalize_pulse = True
-
-            axs[driver_ind].plot(signal_t * 1e9, np.real(signal), label=env.name)
-
-    for driver in range(n_drivers):
-        ax = axs[driver]
-
-        ax.plot(signal_t * 1e9, np.real(res_signal[driver]),
-                label='Resulting signal', c='k', linewidth=2, linestyle='dashed')
-
-        ax.set_title(f'Driver {driver + 1}')
-        ax.grid()
-        ax.set_xlabel('Time[ns]')
-        ax.set_ylabel('Signal[V]')
-        ax.set_xlim(0, t_final * 1e9)
-        if disp_legend:
-            ax.legend()
-
-    plt.suptitle('Real part of pulses')
 
 
 def get_qubits_population(population: np.array, dims: List[int]) -> np.array:
@@ -168,52 +90,6 @@ def get_qubits_population(population: np.array, dims: List[int]) -> np.array:
     return qubitsPopulations
 
 
-def plot_splitted_population(exp: Experiment, psi_init: tf.Tensor, sequence: List[str]) -> None:
-    """
-    Plots time dependent populations for multiple qubits in separate plots.
-    Parameters
-    ----------
-    exp: Experiment
-        The experiment containing the model and propagators
-    psi_init: np.array
-        Initial state vector
-    sequence: List[str]
-        List of gate names that will be applied to the state
-    -------
-    """
-    # calculate the time dependent level population
-    model = exp.pmap.model
-    dUs = exp.partial_propagators
-    psi_t = psi_init.numpy()
-    pop_t = exp.populations(psi_t, model.lindbladian)
-    for gate in sequence:
-        for du in dUs[gate]:
-            psi_t = np.matmul(du, psi_t)
-            pops = exp.populations(psi_t, model.lindbladian)
-            pop_t = np.append(pop_t, pops, axis=1)
-    dims = [s.hilbert_dim for s in model.subsystems.values()]
-    splitted = get_qubits_population(pop_t, dims)
-
-    # timestamps
-    dt = exp.ts[1] - exp.ts[0]
-    ts = np.linspace(0.0, dt * pop_t.shape[1], pop_t.shape[1])
-
-    # create both subplots
-    titles = list(exp.pmap.model.subsystems.keys())
-    fig, axs = plt.subplots(1, len(splitted), sharey="all")
-    for idx, ax in enumerate(axs):
-        ax.plot(ts / 1e-9, splitted[idx].T)
-        ax.tick_params(direction="in", left=True, right=True, top=False, bottom=True)
-        ax.set_xlabel("Time [ns]")
-        ax.set_ylabel("Population")
-        ax.set_title(titles[idx])
-        ax.legend([str(x) for x in np.arange(dims[idx])])
-        ax.grid()
-
-    plt.tight_layout()
-    plt.show()
-
-
 def get_params_dict(opt_params: set, t_final: float) -> dict:
     def_params = {
         'amp': Quantity(value=1e-5, min_val=0.0, max_val=500., unit="V"),
@@ -237,18 +113,6 @@ def get_params_dict(opt_params: set, t_final: float) -> dict:
 def get_opt_params_conf(driver: str, gate_key: str, env_name, env_to_opt_params: set) -> list:
     PARAMS_TO_EXCLUDE = {'t_final', 'sigma'}
     return [[(gate_key, driver, env_name, param), ] for param in env_to_opt_params - PARAMS_TO_EXCLUDE]
-
-
-def get_init_state(exp: Experiment, energy_level: int = None) -> tf.Tensor:
-    dims = exp.pmap.model.dims
-    n_lvls = reduce(lambda x, y: x * y, dims)
-
-    if energy_level is None:
-        energy_level = 0
-
-    psi_init = [[0] * n_lvls]
-    psi_init[0][energy_level] = 1
-    return tf.transpose(tf.constant(psi_init, tf.complex128))
 
 
 def opt_single_sig_exp(exp: Experiment) -> tuple:
