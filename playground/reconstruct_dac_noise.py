bfb7ca28995d3dabc5934992917ec1f4146441a6
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2022-11-09 08:44:48 +0000

    WIP reconstructing DAC noise

---
+++ playground/reconstruct_dac_noise.py
@@ -0,0 +1,47 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+noise_coords = np.array([(78, 185), (104, 238), (162, 286), (193, 285), (307, 348), (392, 384), (547, 393),
+                         (705., 394), (861, 402), (934, 424)])
+spike_coords = np.array([(797, 309), (824, 335), (622, 361), (343, 153.)])
+
+
+def translate_coords_to_data(coords: np.ndarray, interp: bool = True, n_points: int = 10 ** 4, BOTTOM_LEFT=(78, 578)):
+    coords[:, 0] -= BOTTOM_LEFT[0]
+    coords[:, 1] = BOTTOM_LEFT[1] - coords[:, 1]
+
+    x_unit = 235 - 78
+    y_unit = 0.5 * (578 - 157)
+
+    coords[:, 0] /= x_unit
+    coords[:, 1] /= y_unit
+
+    if interp:
+        log_freqs = np.linspace(coords[:, 0].min(), coords[:, 0].max(), n_points)
+        log_amps_per_sqrt_hz = np.interp(log_freqs, coords[:, 0], coords[:, 1])
+    else:
+        log_freqs = coords[:, 0]
+        log_amps_per_sqrt_hz = coords[:, 1]
+
+    freqs = 10 ** log_freqs
+    amps_per_sqrt_hz = 10 ** log_amps_per_sqrt_hz
+
+    return freqs, amps_per_sqrt_hz
+
+
+freqs, amps_per_sqrt_hz = translate_coords_to_data(noise_coords)
+spike_freqs, spike_amps_per_sqrt_hz = translate_coords_to_data(spike_coords, interp=False)
+
+for spike_freq, spike_amp in zip(spike_freqs, spike_amps_per_sqrt_hz):
+    spike_freq_ind = np.argmin(abs(freqs - spike_freq))
+    amps_per_sqrt_hz[spike_freq_ind] = spike_amp
+
+plt.plot(freqs * 1e6, amps_per_sqrt_hz)
+plt.semilogx()
+plt.semilogy()
+
+plt.xlabel('Frequency[MHz]')
+plt.ylabel('Voltage[nV/$\sqrt{Hz}$]')
+plt.title('DAC noise power spectral density')
+
+plt.show()
