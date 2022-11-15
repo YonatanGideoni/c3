61c1251eab2627e34e002402a75fb257902f8b1d
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2022-11-15 16:10:08 +0000

    Got noise time signal

--- playground/reconstruct_dac_noise.py
+++ playground/reconstruct_dac_noise.py
@@ -50,17 +50,54 @@ linsp_amps_per_sqrt_hz = np.interp(linspace_freqs, freqs, amps_per_sqrt_hz)
 
 linsp_amps_per_sqrt_hz *= 1e-9
 signal_psd = linsp_amps_per_sqrt_hz ** 2
-freqs = linspace_freqs
 
-N = len(freqs)
+N = len(linspace_freqs)
 phase = 2 * np.pi * np.random.randn(N)
 magnitude = np.sqrt(signal_psd)
 FFT = magnitude * np.exp(1j * phase)
 
 plt.figure()
-plt.plot(freqs, np.abs(FFT))
-plt.plot(freqs, magnitude)
+plt.plot(linspace_freqs, np.abs(FFT))
+plt.plot(linspace_freqs, magnitude)
 plt.title('DAC Noise PSD')
 plt.ylabel('Voltage[V/$\sqrt{Hz}$]')
+
+delta_f = max(freqs)
+inv_samp_rate = 1 / delta_f
+dt = inv_samp_rate
+
+
+# taken from https://stackoverflow.com/questions/67085963/generate-colors-of-noise-in-python
+def noise_psd(N, psd=lambda f: 1, d=1):
+    X_white = np.fft.rfft(np.random.randn(N))
+    S = psd(np.fft.rfftfreq(N, d))
+    # Normalize S
+    S = S / np.sqrt(np.mean(S ** 2))
+    X_shaped = X_white * S
+    return np.fft.irfft(X_shaped)
+
+
+def PSDGenerator(d=1):
+    def psd_generator(f):
+        return lambda N: noise_psd(N, f, d)
+
+    return psd_generator
+
+
+@PSDGenerator(inv_samp_rate)
+def get_DAC_noise(f: np.ndarray) -> np.ndarray:
+    return np.interp(f, freqs, amps_per_sqrt_hz)
+
+
+def get_noise_time_signal(n_samples: int) -> tuple:
+    ts = dt * np.arange(n_samples)
+    return ts, get_DAC_noise(n_samples)
+
+
+ts, noise = get_noise_time_signal(1000)
+plt.figure()
+plt.plot(ts, noise)
+plt.xlabel('t')
+plt.ylabel('Amplitude[V]')
 
 plt.show()
