735af525042efb5b84b9ba5d0f1fd399f6705a79
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2022-12-17 10:01:55 +0000

    Delete experiments with bad fidelity

--- playground/debug_brute_force_opt.py
+++ playground/debug_brute_force_opt.py
@@ -3,8 +3,7 @@ import os
 from matplotlib import pyplot as plt
 
 from c3.experiment import Experiment
-from playground.brute_force_opt_gate import read_cached_opt_map_params, find_opt_env_for_gate, calc_exp_fid, \
-    get_alex_system
+from playground.brute_force_opt_gate import read_cached_opt_map_params, find_opt_env_for_gate, calc_exp_fid
 from playground.plot_utils import plot_dynamics, get_init_state, wait_for_not_mouse_press, plot_signal, \
     plot_splitted_population
 
@@ -18,7 +17,7 @@ def run_gate_exp_opt(exp_path: str, exp_cache_dir, debug_cache_dir='debug_cache'
     find_opt_env_for_gate(exp, gate, opt_map_params, cache_dir=debug_cache_dir, debug=True)
 
 
-def plot_exps_in_dir(dir_path: str, min_plot_fid: float = 0.):
+def plot_exps_in_dir(dir_path: str, min_plot_fid: float = 0., delete_bad_res: bool = False):
     for file_name in os.listdir(dir_path):
         if file_name.split('.')[-1] != 'hjson':
             continue
@@ -26,6 +25,8 @@ def plot_exps_in_dir(dir_path: str, min_plot_fid: float = 0.):
         exp_path = os.path.join(dir_path, file_name)
         exp = Experiment()
         exp.read_config(exp_path)
+        for qubit in exp.pmap.model.subsystems.values():
+            qubit.hilbert_dim = 6
         exp.compute_propagators()
 
         fid = calc_exp_fid(exp)
@@ -35,7 +36,13 @@ def plot_exps_in_dir(dir_path: str, min_plot_fid: float = 0.):
         print(f'Fid={fid:.4f}')
 
         if fid < min_plot_fid:
+            if delete_bad_res:
+                os.remove(exp_path)
             continue
+
+        exp = Experiment()
+        exp.read_config(exp_path)
+        exp.compute_propagators()
 
         init_state = get_init_state(exp, energy_level=4)
         gate = list(exp.pmap.instructions.values())[0]
@@ -59,16 +66,16 @@ def plot_exps_in_dir(dir_path: str, min_plot_fid: float = 0.):
         plt.close('all')
 
 
-def plot_good_results(base_dir: str, min_plot_fid: float = 0):
+def plot_good_results(base_dir: str, min_plot_fid: float = 0, delete_bad_res: bool = False):
     for subdir in os.listdir(base_dir):
         subdir_path = os.path.join(base_dir, subdir)
         if not os.path.isdir(subdir_path):
             continue
 
         if subdir != 'good_infid_exps':
-            plot_good_results(subdir_path, min_plot_fid)
+            plot_good_results(subdir_path, min_plot_fid, delete_bad_res)
         else:
-            plot_exps_in_dir(subdir_path, min_plot_fid)
+            plot_exps_in_dir(subdir_path, min_plot_fid, delete_bad_res)
 
 
 if __name__ == '__main__':
